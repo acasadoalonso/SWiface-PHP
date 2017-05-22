174e0473fd5755ce0a511fe9814380cb259e5dbf
Angel Casado <acasado@acm.org>
2017-05-22 19:39:04 +0200

    add sgp2sws page and script

--- sgp2sws.php
+++ sgp2sws.php
@@ -1,9 +1,9 @@
 <?php
-$compid = $_GET['compid'];
-$indexday = $_GET['indexday'];
+$compid = $_POST['compid'];
+$indexday = $_POST['indexday'];
 $cwd =getcwd();
 $rc=0;
-echo $compid.' '.$indexday;
+echo 'COMPid='.$compid.' Indexday='.$indexday;
 ob_start();
 passthru('/usr/bin/python2.7 '.$cwd.'/sgp2sws.py '.$compid.' '.$indexday, $rc);
 $output = ob_get_clean(); 
