55034f4298ba39256fe527091e251398abf395a8
Angel Casado <acasadoalonso@gmail.com>
2019-06-10 18:43:09 +0200

    adds from SGP Final

--- utils/ts.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import time
-import sys
-since  =sys.argv[1]
-datetimes=datetime.datetime.utcfromtimestamp(int(since))
-date=     datetimes.strftime("%y%m%d")
-time=     datetimes.strftime("%H%M%S")
-print date, time
