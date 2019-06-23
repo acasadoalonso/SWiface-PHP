30ab562e89623fab92e01915d1341e1ac9d4076c
Angel Casado <acasadoalonso@gmail.com>
2019-06-23 12:48:09 +0200

    improvement on dir2fil

--- dir2fil.py
+++ dir2fil.py
@@ -35,6 +35,7 @@ prtreq   =sys.argv[2:]					# print request
 stats={}                                                # statistics 
 DBpath=config.DBpath		        		# directory where to store IGC directory 
 dirpath=DBpath+"IGCfiles"                               # directory where will got the IGC files
+tmppath=DBpath+"TMP/"                                   # directory where will got the IGC files
 
 #
 
@@ -51,29 +52,33 @@ hostname=socket.gethostname()
 print "DBhost:", config.DBhost, "ServerName:", hostname
 start_time = time.time()
 local_time = datetime.datetime.now()
-print "================================================\n\n"
+print "Extracting FLARM info from files at: ", dirpath
+print "==============================================================\n\n"
 
 
 if not os.path.isdir(dirpath):                          # check that we have such directory
     print "Not IGC directory: ", dirpath, "\n\n"
     exit(-1)                                            # nothing else to do 
-os.system("rm "+DBpath+"/TMP/*")                        # remove and clean the working directory 
+if not os.path.isdir(tmppath):                          # if the working directory does not exists ??
+    os.system ("mkdir "+tmppath)                        # make it !!!
+else:
+    os.system("rm "+tmppath+"/*")                       # remove and clean the working directory 
 ld = os.listdir(dirpath)                                # get the list of files 
 cnt = 0                                                 # count of number of records processed
 for f in ld:                                            # scan all the files on the from directory
     fd=open(dirpath+"/"+f, 'r')                         # open the file
-    cnt +=getflarmfile(fd, f, DBpath+"/TMP/"+f, stats)  # extract the FLARM data from the embeded records
+    cnt +=getflarmfile(fd, f, tmppath+f, stats)         # extract the FLARM data from the embeded records
     fd.close()                                          # close the file
-cwd=os.getcwd()
-print "From CD:", cwd, "To:", DBpath+"/TMP/"            # report it
-os.chdir(DBpath+"/TMP/")                                # report current directory and the new one
-fname=FlarmID+'.'+getognreg(FlarmID)+'.'+getogncn(FlarmID)+'.igc'
+cwd=os.getcwd()                                         # remember the current directory
+print "From CD:", cwd, "To:", tmppath                   # report it
+os.chdir(tmppath)                                       # report current directory and the new one
+fname=FlarmID+'.'+getognreg(FlarmID)+'.'+getogncn(FlarmID)+'.igc'   # file name of the rebuilt file
 if os.path.isfile(fname):                               # remove to avoid errors
     os.remove(fname)                                    # remove if exists
                                                         # get the new IGC files based on the FLARM messages
 os.system('grep "FLARM "'+FlarmID+' * | sort -k 3 | python '+cwd+'/genIGC.py '+FlarmID+' > '+fname)
 
-print "New IGC rebuilt file:", fname, " is at:", DBpath+"/TMP/", "\n\n"
+print "New IGC rebuilt file:", fname, " is at:", tmppath, "\n\n"
 print "Records processed:",cnt, "\n\nStats:", stats
 
 
