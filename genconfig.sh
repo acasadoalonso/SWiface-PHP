fe5af38d58fd844ef7f2f3cefa0bf91cddd112f9
acasadoalonso <acasado@acm.org>
2026-08-18 18:46:32 +0200

    set the chown for genconfig

--- genconfig.sh
+++ genconfig.sh
@@ -3,9 +3,10 @@
 if [ -n "$CONFIGDIR" ]; then
     echo "CONFIGDIR is set to: $CONFIGDIR"
     python3 gen_swsconfig.py  --comp-id $1 --config-dir $CONFIGDIR
+    chown www-data:www-data $CONFIGDIR/* 
 else
     echo "CONFIGDIR is not set, use the default /etc/local directory"
     python3 gen_swsconfig.py  --comp-id $1 --config-dir /etc/local 
 fi
-
+# gen the config.py and config.php files
 python3 genconfig.py
