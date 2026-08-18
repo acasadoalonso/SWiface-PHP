314820ab5c0ddb110564ea9333c45b2b6031a013
acasadoalonso <acasado@acm.org>
2026-08-18 18:29:10 +0200

    add the test of CONFIGDIR

--- genconfig.sh
+++ genconfig.sh
@@ -1,2 +1,11 @@
-python3 gen_swsconfig.py  --comp-id $1 --config-dir /etc/local 
+#!/bin/bash
+# usage: ./genconfig.sh <comp-id>   where <comp-id> is the CSGP competition ID 
+if [ -n "$CONFIGDIR" ]; then
+    echo "CONFIGDIR is set to: $CONFIGDIR"
+    python3 gen_swsconfig.py  --comp-id $1 --config-dir $CONFIGDIR
+else
+    echo "CONFIGDIR is not set, use the default /etc/local directory"
+    python3 gen_swsconfig.py  --comp-id $1 --config-dir /etc/local 
+fi
+
 python3 genconfig.py
